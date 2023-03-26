ef438f3a69ac91016e8566afa7733c47f477ab78
xiaowen66621 <18264480900@163.com>
2023-03-27 00:14:28 +0800

    feat: layout

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { DatePicker, Button } from 'antd';
-import { StarOutlined, StarFilled, StarTwoTone } from '@ant-design/icons';
-import { Skeleton } from 'antd';
+
 
 const Login: React.FC = () => {
   return (
     <div style={{ height: '100vh', width: '100vw' }} >
-      <DatePicker />
-      <Button type="primary">Primary Button</Button>
 
-      <StarOutlined />
-      <StarFilled />
-      <StarTwoTone twoToneColor="#eb2f96" />
-      <Skeleton avatar active paragraph={{ rows: 4 }} />
     </div>
   )
 }
