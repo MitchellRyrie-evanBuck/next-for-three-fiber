3c49894149b299d7ca35fa49d471ef98dbc2cdd1
af-liu <18264480900@163.com>
2023-07-12 23:44:59 +0800

    fix: windcss

--- next.config.js
+++ next.config.js
@@ -4,31 +4,32 @@ const withCSS = require('@zeit/next-css')
 const withLess = require('@zeit/next-less')
 const withSass = require("@zeit/next-sass");
 
-const CSS = withCSS({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[local]___[hash:base64:5]",
-  },
-  ...withLess(
-    withSass({
-      lessLoaderOptions: {
-        javascriptEnabled: true,
-      },
-    })
-  ),
-});
+// const CSS = withCSS({
+//   cssModules: true,
+//   cssLoaderOptions: {
+//     importLoaders: 1,
+//     localIdentName: "[local]___[hash:base64:5]",
+//   },
+//   ...withLess(
+//     withSass({
+//       lessLoaderOptions: {
+//         javascriptEnabled: true,
+//       },
+//     })
+//   ),
+// });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+  reactStrictMode: false,
+  // sassOptions: {
+    // includePaths: [path.join(__dirname, 'styles')],
+  // },
   webpack: config => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   },
+
 }
 
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+// import styles from '@/styles/Home.module.css'
+// import '@/styles/globals.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      --
+      <div className="bg-blue-500 text-white p-4">
+        This is a blue div with white text.
+      </div>
     </>
   )
 }
