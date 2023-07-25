155fe467096c2980b21169b2152a774a56b4cafc
af-liu <18264480900@163.com>
2023-07-25 16:18:03 +0800

    feat: 取消SplitText3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       headings = gsap.utils.toArray(".section-heading"),
       outerWrappers = gsap.utils.toArray(".outer"),
       innerWrappers = gsap.utils.toArray(".inner"),
-      splitHeadings = headings.map(heading => new SplitText((heading as any), { type: "chars,words,lines", linesClass: "clip-text" })),
+      // splitHeadings = headings.map(heading => new SplitText((heading as any), { type: "chars,words,lines", linesClass: "clip-text" })),
       currentIndex = -1,
       wrap = gsap.utils.wrap(0, sections.length),
       animating: boolean;
@@ -52,19 +52,19 @@ export default function Home() {
         yPercent: 0
       }, 0)
         .fromTo(images[index], { yPercent: 15 * dFactor }, { yPercent: 0 }, 0)
-        .fromTo(splitHeadings[index].chars, {
-          autoAlpha: 0,
-          yPercent: 150 * dFactor
-        }, {
-          autoAlpha: 1,
-          yPercent: 0,
-          duration: 1,
-          ease: "power2",
-          stagger: {
-            each: 0.02,
-            from: "random"
-          }
-        }, 0.2);
+        // .fromTo(splitHeadings[index].chars, {
+        //   autoAlpha: 0,
+        //   yPercent: 150 * dFactor
+        // }, {
+        //   autoAlpha: 1,
+        //   yPercent: 0,
+        //   duration: 1,
+        //   ease: "power2",
+        //   stagger: {
+        //     each: 0.02,
+        //     from: "random"
+        //   }
+        // }, 0.2);
       currentIndex = index;
     }
     const observer = Observer.create({
