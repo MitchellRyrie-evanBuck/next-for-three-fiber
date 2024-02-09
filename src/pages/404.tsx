d3298b76cf7b5970e9c0cadbe43905163d917fc3
af-liu <18264480900@163.com>
2024-02-09 22:48:02 +0800

    feat: 暂存

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ export default function NotFind() {
   return (
     <>
       <div className=" text-black font-700 text-4xl pt-96 h-full w-full text-center">
-        404 Not Find
+         Not Find
       </div>
     </>
   )
