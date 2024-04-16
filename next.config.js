ade20845cf2422fd0d745dd0165558c4d12d1e84
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-04-17 00:03:29 +0800

    feat config 更改

--- next.config.js
+++ next.config.js
@@ -6,11 +6,6 @@ const path = require('path');
  */
 const nextConfig = {
   reactStrictMode: false,
-  // lessLoaderOptions: {
-  //   javascriptEnabled: true,
-  //   includePaths: [path.join(__dirname, 'styles')],
-  // },
-  // transpilePackages: ['three'],
   distDir: 'build',
   publicRuntimeConfig: {
     staticFolder: '/static',
@@ -21,49 +16,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
-  experimental:{
-    appDir: false,
-    // fastRefresh: false,
-  },
-  swcMinify: false,
   webpack: (config, options) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
-    // if (!options.isServer) {
-    //   config.module.rules.push({
-    //     test: /three\.module\.js$/,
-    //     loader: 'babel-loader', // 使用默认的 Babel 转译器
-    //   });
-    // }
-    // config.module.rules.push({
-    //   test: /\.css$/,
-    //   use: ['style-loader', 'css-loader'],
-    // });
-
-    // config.module.rules.push({
-    //   test: /\.less$/,
-    //   use: ['style-loader', 'css-loader', 'less-loader'],
-    // });
     config.module.rules.push({
       test: /\.md$/,
       use: ['raw-loader', 'remark-loader'],
     });
-
-    config.module.rules.push(
-      // {
-      //   test: /\.(gif|svg)$/i,
-      //   use: [
-      //     {
-      //      options: {
-      //         limit: config.inlineImageLimit,
-      //         publicPath: `${config.assetPrefix || '/_next/static/'}`,
-      //         outputPath: `${options.isServer ? "../" : ""}static/`,
-      //       }
-      //     }
-      //   ]
-      // },
-    )
-
-
     return config;
   },
   sassOptions: {
