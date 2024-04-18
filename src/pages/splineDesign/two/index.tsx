3505fa23419919a048d74c5049926b622288851d
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-04-18 23:00:41 +0800

    feat:. 素材

---
+++ src/pages/splineDesign/two/index.tsx
@@ -0,0 +1,8 @@
+
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/SPj6kherRZeO6o91/scene.splinecode" />
+  );
+}
