217c82a2983af3ae2fc54f3c82c97a7182ff8dee
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-06-25 23:15:08 +0800

    暂存

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -23,23 +23,23 @@ export default function Home() {
   return (
     <>
       <div className={``}>
-        {/* <motion.h1
+        <motion.h1
           className="typed-text text-center font-600 text-3xl pt-40 h-140px"
           initial={{ opacity: 0, y: -10 }}
           animate={animationControls}
         >
           {typedStrings[currentString]}
-        </motion.h1> */}
-        {/* <div className='text-center font-600 text-2xl best'>
+        </motion.h1>
+        <div className='text-center font-600 text-2xl best'>
           My name is Anthony Liu, and I am a web developer who enjoys exploring unknown fields.
         </div>
         <div className='best text-center font-600 text-2xl'>
           I am glad you can see my blog and wish you a happy day.
-        </div> */}
-        <div className='w-1/2 h-1/2 mx-auto mt-20'>
-          <Spline scene="https://prod.spline.design/zQBhbbwDW5Mn8L1T/scene.splinecode" />
-          {/* <Spline scene="https://prod.spline.design/2V3oJsyoivobyNme/scene.splinecode" /> */}
         </div>
+        {/* <div className='w-1/2 h-1/2 mx-auto mt-20'> */}
+          {/* <Spline scene="https://prod.spline.design/zQBhbbwDW5Mn8L1T/scene.splinecode" /> */}
+          {/* <Spline scene="https://prod.spline.design/2V3oJsyoivobyNme/scene.splinecode" /> */}
+        {/* </div> */}
       </div>
     </>
   );
