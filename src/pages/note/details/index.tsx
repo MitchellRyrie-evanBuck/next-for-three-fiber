5c019b137f363e39c522325f8f83a6e2e36d1c0f
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-07-29 15:56:55 +0800

    fix: bug build

--- src/pages/note/details/index.tsx
+++ src/pages/note/details/index.tsx
@@ -1,7 +1,6 @@
 
 import React, { FC, useEffect, useState } from 'react';
 import MarkdownDocs from '@/docs/webpack/构建流程.md'
-import MarkdownContent from '@/components/markdown/MarkdownContent';
 
 
 type InProps = {
@@ -12,7 +11,6 @@ const Details: FC<InProps> = () => {
   return (
     <div className='w-1/2 m-auto p-10 bg-slate-300'>
     ----
-      <MarkdownContent content={MarkdownDocs} />
     </div>
   )
 }
